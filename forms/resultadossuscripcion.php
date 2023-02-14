37ca101944dcb0420ed7fb0e0274c2d01b086569
punchis <pablo.garcia.bermejo.lopez@gmail.com>
2023-02-14 19:04:39 +0100

    php

---
+++ forms/resultadossuscripcion.php
@@ -0,0 +1,14 @@
+<?php
+
+
+$nombre=$_POST ['nombre'];
+$email=$_POST ['email'];
+$telefono=$_POST ['telefono'];
+
+echo ('El nombre es '.$nombre);
+echo ("<br>");
+echo $email;
+echo ("<br>");
+echo $telefono;
+echo ("<br>");
+?>
