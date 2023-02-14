0527a6b4f9f589a1ca5e5feb55dda28033ab3ad6
lobo409 <114610913+lobo409@users.noreply.github.com>
2023-02-14 18:09:39 +0100

    resultado_colabora.php

--- result_form/resultado_colabora.php
+++ result_form/resultado_colabora.php
@@ -4,9 +4,15 @@ $apellido=$_POST ['apellido'];
 $telefono=$_POST ['telefono'];
 $email=$_POST ['email'];
 $direccion=$_POST ['direccion'];
-$servicio1=$_POST ['servicio1'];
-$servicio2=$_POST ['servicio2'];
-$servicio3=$_POST ['servicio3'];
+if(isset($_POST['servicio1'])){
+    $servicio1=$_POST ['servicio1'];
+}
+if(isset($_POST['servicio2'])){
+    $servicio2=$_POST ['servicio2'];
+}
+if(isset($_POST['servicio3'])){
+    $servicio3=$_POST ['servicio3'];
+}
 $imagen=$_POST ['imagen'];
 
 echo ('El nombre es '.$nombre);
@@ -18,12 +24,18 @@ echo ("<br>");
 echo $email;
 echo ("<br>");
 echo $direccion;
+if(isset($_POST['servicio1'])){
 echo ("<br>");
 echo $servicio1;
+}
+if(isset($_POST['servicio2'])){
 echo ("<br>");
 echo $servicio2;
+}
+if(isset($_POST['servicio3'])){
 echo ("<br>");
 echo $servicio3;
+}
 echo ("<br>");
 echo $imagen;
 ?>
