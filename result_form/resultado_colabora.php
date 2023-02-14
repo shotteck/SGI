f07add46d2c1275aa89f85586cf12cca1ac82c21
lobo409 <114610913+lobo409@users.noreply.github.com>
2023-02-14 17:43:37 +0100

    resultado_colabora.php

--- result_form/resultado_colabora.php
+++ result_form/resultado_colabora.php
@@ -1,1 +1,28 @@
+$nombre=$_POST ['nombre'];
+$apellido=$_POST ['apellido'];
+$telefono=$_POST ['telefono'];
+$email=$_POST ['email'];
+$direccion=$_POST ['direccion'];
+$servicio1=$_POST ['servicio1'];
+$servicio2=$_POST ['servicio2'];
+$servicio3=$_POST ['servicio3'];
+$imagen=$_POST ['imagen'];
 
+echo ('El nombre es '.$nombre);
+echo ("<br>");
+echo $apellido;
+echo ("<br>");
+echo $telefono;
+echo ("<br>");
+echo $email;
+echo ("<br>");
+echo $direccion;
+echo ("<br>");
+echo $servicio1;
+echo ("<br>");
+echo $servicio2;
+echo ("<br>");
+echo $servicio3;
+echo ("<br>");
+echo $imagen;
+?>
